45ca89c85a07aadb4576f0c1fcf58defc7412c25
NundaElroy <sekyezaelroy@gmail.com>
2025-01-17 09:28:01 +0300

    first commit

---
+++ src/main/java/com/gohaibo/gohaibo/GohaiboApplication.java
@@ -0,0 +1,13 @@
+package com.gohaibo.gohaibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GohaiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GohaiboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gohaibo/gohaibo/GohaiboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gohaibo.gohaibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GohaiboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
